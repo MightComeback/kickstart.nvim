8432bad1c9420c59336867e83f0d2900268bbeaf
MightComeback <kuznetsovivan496@gmail.com>
2024-06-22 14:41:35 +0300

    feat: copilot + colorscheme

--- init.lua
+++ init.lua
@@ -167,6 +167,8 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagn
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Custom keymaps
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
@@ -789,6 +791,13 @@ require('lazy').setup({
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
+  },
+
+  -- Copilot
+  {
+    {
+      'github/copilot.vim',
+    },
   },
 
   -- Highlight todo, notes, etc in comments
